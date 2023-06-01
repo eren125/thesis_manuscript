7e7b3d9955770cba157259511fa6ea9245cbdc5b
eren125 <emmanuelren1995@gmail.com>
2023-06-01 12:04:29 +0200

    update

--- formatting.sh
+++ formatting.sh
@@ -7,6 +7,18 @@
 # sed -i '' 's/i\.e\. /i\.e\.\\ /g' $1
 # sed -i '' 's/ \\cite/~\\cite/g' $1
 
+sed -i  's/{Jan}/Jan/g' thesis.bib
+sed -i  's/{Fev}/Fev/g' thesis.bib
+sed -i  's/{Mar}/Mar/g' thesis.bib
+sed -i  's/{Apr}/Apr/g' thesis.bib
+sed -i  's/{May}/May/g' thesis.bib
+sed -i  's/{Jun}/Jun/g' thesis.bib
+sed -i  's/{Jul}/Jul/g' thesis.bib
+sed -i  's/{Aug}/Aug/g' thesis.bib
+sed -i  's/{Sep}/Sep/g' thesis.bib
+sed -i  's/{Oct}/Oct/g' thesis.bib
+sed -i  's/{Nov}/Nov/g' thesis.bib
+sed -i  's/{Dec}/Dec/g' thesis.bib
 sed -i  's/{jan}/Jan/g' thesis.bib
 sed -i  's/{fev}/Fev/g' thesis.bib
 sed -i  's/{mar}/Mar/g' thesis.bib
